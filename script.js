3316d158ea685b94fe50adbc7f9f012d652b357e
maximus82190 <maxelandaniels@gmail.com>
2022-11-08 10:12:58 +0200

    Create buttons and make functional

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+const deskTop = document.getElementById("deskTop");
+const mobile = document.getElementById("mobileVideo");
+const vidGrid = document.getElementById("videoGrid");
+let isSmallScreen = window.screen.width < 600;
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+if (isSmallScreen) {
+  deskTop.style.display = "none";
+} else {
+  vidGrid.style.display = "none";
+}
+
 //tan = function getTanFromDegrees(degrees) {
 //  return Math.tan((degrees * Math.PI) / 180);
 //};
